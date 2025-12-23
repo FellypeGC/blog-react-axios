2e1f0f6db9396e1b9d2f70f77b7bb83198c5f6e1
FellypeGC <gabrielfellypesteve@gmail.com>
2025-12-23 15:19:26 -0300

    catching API data and displaying posts on Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import "./Home.css"
+import { useState, useEffect } from 'react'
+
+import { Link } from 'react-router-dom'
+
+import axios from 'axios';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/todos")
+      const data = response.data;
+      
+      setPosts(data)
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais...</Link>
+          </div>
+        ))
+      )}
+    </div>
   )
 }
 
