0c07f8f82d7430321fee68c9ec288dc0007a4bb3
FellypeGC <gabrielfellypesteve@gmail.com>
2025-12-23 12:33:44 -0300

    first commit and react router added

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+// páginas
+import Home from './routes/Home.jsx'
+import NewPost from './routes/NewPost.jsx'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {  
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/new",
+        element: <NewPost />
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
