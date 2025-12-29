91551b4a6679c3b058bca24d51aa3a30ca4bf131
FellypeGC <gabrielfellypesteve@gmail.com>
2025-12-29 15:29:40 -0300

    admin page has been added succefully

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,6 +15,9 @@ const NavBar = () => {
         <li>
           <Link to={`/new`} className='new-btn'>Novo Post</Link>
         </li>
+        <li>
+          <Link to={`/admin`}>Gerenciar</Link>
+        </li>
       </ul>
     </nav>
   )
