90cc6368ec30e267e1dd48771aaeea47e893fb94
FellypeGC <gabrielfellypesteve@gmail.com>
2025-12-30 12:06:26 -0300

    creating post editing page

---
+++ src/routes/EditPost.jsx
@@ -0,0 +1,45 @@
+import "./NewPost.css"
+
+import blogFetch from "../axios/config"
+
+import { useState, useEffect } from "react"
+
+import { useParams, useNavigate } from "react-router-dom"
+
+const EditPost = () => {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+
+  const { id } = useParams();
+
+  return (
+    <div className='new-post'>
+      <h2>Editando: {title}</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input 
+            type="text" 
+            name='title' 
+            placeholder='Digite o título' 
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea 
+            name="body" 
+            id="body" 
+            placeholder='Digite o conteúdo'
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+        </div>
+        <input type="submit" value="Editar Post" className='btn' />
+      </form>
+    </div>
+  )
+}
+
+export default EditPost
